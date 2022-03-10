483de7e9c2414c5d0fb9d23ca95c547782622be0
mogitozhang <mogitozhang@tencent.com>
2022-03-10 17:13:05 +0800

    Update

--- log/log.go
+++ log/log.go
@@ -5,7 +5,7 @@ import "github.com/sirupsen/logrus"
 var Logger = logrus.New()
 
 func init() {
-	Logger.SetLevel(logrus.DebugLevel)
-	// Logger.SetLevel(logrus.ErrorLevel)
+	// Logger.SetLevel(logrus.DebugLevel)
+	Logger.SetLevel(logrus.ErrorLevel)
 	Logger.SetFormatter(&logrus.TextFormatter{})
 }
